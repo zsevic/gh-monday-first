872def45c7338f14e1ebabb140482501285aaca8
zsevic <zsevic95@gmail.com>
2017-11-23 00:02:48 +0100

    :rocket: shift name of the days

---
+++ script.js
@@ -0,0 +1,13 @@
+const gh = document.querySelector('.js-calendar-graph')
+if (gh) {
+  console.log('gh profile')
+  const weekDays = document.querySelectorAll('.wday')
+  const days = []
+  weekDays.forEach((day) => {
+    days.push(day.innerHTML)
+  })
+  days.push(days.shift())
+  weekDays.forEach((day, index) => {
+    day.innerHTML = days[index]
+  })
+}
