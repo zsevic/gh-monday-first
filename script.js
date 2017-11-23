7e2a18002a7062477b18ab2779462a5a8060e91e
zsevic <zsevic95@gmail.com>
2017-11-23 21:14:33 +0100

    :bulb: add comments

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gh = document.querySelector('.js-calendar-graph')
+// check if the user is on some Github profile's page
 if (gh) {
   console.log('gh profile')
   const weekDays = document.querySelectorAll('.wday')
+  // shift names of the days on the left side
   shiftNames(weekDays)
   const yearDays = document.querySelectorAll('.day')
   shiftDays(yearDays)
@@ -10,12 +12,15 @@ if (gh) {
 function shiftDays (yearDays) {
   yearDays.forEach((day, index) => {
     if (index % 7 === 0) {
+      // remove first Sunday
       if (index === 0) {
         day.style['display'] = 'none'
+      // shift Sunday box to the bottom and translate it to the left
       } else {
         day.y.baseVal.value = 72
         day.setAttribute('transform', 'translate(-12,0)')
       }
+    // shift other days to the top
     } else {
       day.y.baseVal.value = day.y.baseVal.value - 12
     }
