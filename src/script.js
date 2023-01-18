e486f61100ab8cb502cd3e509e0cd2ebf8844078
zsevic <zsevic95@gmail.com>
2023-01-18 11:35:08 +0100

    Set up linter and fix lint errors

--- src/script.js
+++ src/script.js
@@ -1,44 +1,44 @@
-const gh = document.querySelector('.js-calendar-graph')
-// check if the user is on some Github profile's page
-if (gh) {
-  console.log('gh profile')
-  const weekDays = document.querySelectorAll('.wday')
-  // shift names of the days on the left side
-  shiftNames(weekDays)
-  const yearDays = document.querySelectorAll('.day')
-  shiftDays(yearDays)
-}
-
-function shiftDays (yearDays) {
+function shiftDays(yearDays) {
   yearDays.forEach((day, index) => {
     if (index % 7 === 0) {
       // remove first Sunday
       if (index === 0) {
-        day.style['display'] = 'none'
+        day.style.display = 'none';
         // shift Sunday box to the bottom and translate it to the left
       } else {
-        day.y.baseVal.value = 78
-        day.setAttribute('transform', 'translate(-13,0)')
+        day.y.baseVal.value = 78;
+        day.setAttribute('transform', 'translate(-13,0)');
       }
       // shift other days to the top
     } else {
-      day.y.baseVal.value = day.y.baseVal.value - 13
+      day.y.baseVal.value -= 13;
     }
-  })
+  });
 }
 
-export default function shiftNames (weekDays) {
-  const days = []
-  weekDays.forEach(day => {
-    days.push(day.innerHTML)
-  })
-  days.push(days.shift())
+export default function shiftNames(weekDays) {
+  const days = [];
+  weekDays.forEach((day) => {
+    days.push(day.innerHTML);
+  });
+  days.push(days.shift());
   weekDays.forEach((day, index) => {
-    day.innerHTML = days[index]
+    day.innerHTML = days[index];
     if (index % 2) {
-      day.style['display'] = 'none'
+      day.style.display = 'none';
     } else {
-      day.style['display'] = 'inline'
+      day.style.display = 'inline';
     }
-  })
+  });
+}
+
+const gh = document.querySelector('.js-calendar-graph');
+// check if the user is on some Github profile's page
+if (gh) {
+  console.log('gh profile');
+  const weekDays = document.querySelectorAll('.wday');
+  // shift names of the days on the left side
+  shiftNames(weekDays);
+  const yearDays = document.querySelectorAll('.day');
+  shiftDays(yearDays);
 }
